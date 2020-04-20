a17e6633724827933e32a7b158c4a7fcced55ac1
YulonSil <richard.alpert.c@gmail.com>
2020-04-21 00:19:01 +0200

    vague start

--- srcs/parsing/get_args.c
+++ srcs/parsing/get_args.c
@@ -6,21 +6,62 @@
 /*   By: cclaude <cclaude@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/20 19:41:37 by cclaude           #+#    #+#             */
-/*   Updated: 2020/04/20 20:07:26 by cclaude          ###   ########.fr       */
+/*   Updated: 2020/04/21 00:18:31 by cclaude          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			return (0);
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+void	arg_type(t_token *token)
+{
+	if (ft_strcmp(token->tok, "") == 0)
+		token->type = EMPTY;
+	else if (ft_strcmp(token->tok, ">") == 0)
+		token->type = APPEND;
+	else if (ft_strcmp(token->tok, ">>") == 0)
+		token->type = TRUNC;
+	else if (ft_strcmp(token->tok, "<") == 0)
+		token->type = REDIR;
+	else if (ft_strcmp(token->tok, "|") == 0)
+		token->type = PIPE;
+	else if (ft_strcmp(token->tok, ";") == 0)
+		token->type = END;
+	else if (token->prev == NULL || token->prev->type >= APPEND)
+		token->type = CMD;
+	else
+		token->type = ARG;
+}
+
 t_token	*get_next(char *cmd_line, int *i)
 {
 	t_token	*token;
+	char	c;
 	int		j;
 
-	j = 0;
 	token = malloc(sizeof(t_token));
-	token->tok = malloc(SIZEOF ARG);
-	while (cmd_line[*i] != ' ')
+	c = ' ';
+	if (cmd_line[*i] == '\'' || cmd_line[*i] == '\"')
+		c = cmd_line[(*i)++];
+	j = 0;
+	while (cmd_line[*i + j] != c)
+		j++;
+	token->tok = malloc(sizeof(char) * (j + 1));
+	j = 0;
+	while (cmd_line[*i] != c)
 		token->tok[j++] = cmd_line[(*i)++];
 	return (token);
 }
@@ -40,9 +81,29 @@ t_token	*get_args(char *cmd_line)
 		if (prev)
 			prev->next = next;
 		prev = next;
+		arg_type(next);
 	}
 	next->next = NULL;
 	while (next->prev)
 		next = next->prev;
 	return (next);
+}
+
+int	main(void)
+{
+	t_token	*list;
+	int		i = 1;
+
+	list = get_args("ls");
+	while (list->next)
+	{
+		printf("#%d %s\n", i, list->tok);
+		free(list->tok);
+		list = list->next;
+		free(list->prev);
+		i++;
+	}
+	printf("#%d %s\n", i, list->tok);
+	free(list->tok);
+	free(list);
 }
--- srcs/parsing/minishell.h
+++ srcs/parsing/minishell.h
@@ -6,7 +6,7 @@
 /*   By: cclaude <cclaude@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/20 19:41:53 by cclaude           #+#    #+#             */
-/*   Updated: 2020/04/20 20:02:59 by cclaude          ###   ########.fr       */
+/*   Updated: 2020/04/21 00:11:37 by cclaude          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,26 @@
 # include <string.h>
 # include <fcntl.h>
 
-# define EMP 0
+# define EMPTY 0
 # define CMD 1
 # define ARG 2
-# define REDIR 3
-# define PIPE 4
-# define ENV 5
-# define END 6
-
-typedef struct	s_mini
-{
-	t_token			*start;
-}				t_mini;
+# define APPEND 3
+# define TRUNC 4
+# define REDIR 5
+# define PIPE 6
+# define END 7
 
 typedef struct	s_token
 {
 	char			*tok;
 	int				type;
-	t_token			*prev;
-	t_token			*next;
+	struct s_token	*prev;
+	struct s_token	*next;
 }				t_token;
+
+typedef struct	s_mini
+{
+	t_token			*start;
+}				t_mini;
 
 #endif
