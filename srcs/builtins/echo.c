0fdce1e7e7a9ce236c25adeeed2fe98e260f4506
macrespo <maxime.crespopro@gmail.com>
2020-06-15 20:16:25 +0200

    no more segfault without args

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,31 +6,46 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/08 17:15:26 by macrespo          #+#    #+#             */
-/*   Updated: 2020/06/08 19:08:06 by macrespo         ###   ########.fr       */
+/*   Updated: 2020/06/15 20:15:39 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		ft_echo(char **args)
+static	int		nb_args(char **args)
+{
+	int		size;
+
+	size = 0;
+	while (args[size])
+		size++;
+	return (size);
+}
+
+int				ft_echo(char **args)
 {
 	int		i;
 	int		n_option;
 
 	i = 1;
 	n_option = 0;
-	if (ft_strcmp(args[1], "-n") == 0)
+	if (nb_args(args) > 1)
 	{
+		if (ft_strcmp(args[1], "-n") == 0)
+
+		{
 			n_option = 1;
+
 			i++;
-	}
-	while (args[i])
-	{
-		ft_putstr_fd(args[i], 1);
-		if (args[i + 1])
-			write(1, " ", 1);
-		i++;
-	}
+		}
+		while (args[i])
+		{
+			ft_putstr_fd(args[i], 1);
+			if (args[i + 1])
+				write(1, " ", 1);
+			i++;
+		}		
+	}	
 	if (n_option == 0)
 		write(1, "\n", 1);
 	return (0);
