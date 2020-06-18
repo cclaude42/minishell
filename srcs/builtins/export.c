d6b1d8f6ef937f322675e963e7d404c81cf81adb
macrespo <maxime.crespopro@gmail.com>
2020-06-18 17:32:29 +0200

    env print last var | export add new vars

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,16 +6,37 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/15 18:15:54 by macrespo          #+#    #+#             */
-/*   Updated: 2020/06/16 17:33:31 by macrespo         ###   ########.fr       */
+/*   Updated: 2020/06/18 17:27:02 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		ft_export(char **args, t_env *env)
+static int		env_add_back(const char *value, t_env *env)
 {
-	(void)args;
-	(void)env;
-	printf("YOURE ARE IN EXPORT BITCH !!\n");
+	t_env	*new;
+	t_env	*tmp;
+
+	if (!(new = malloc(sizeof(t_env*))))
+		return (-1);
+	new->value = ft_strdup(value);
+	while (env && env->next && env->next->next)
+		env = env->next;
+	tmp = env->next;
+	env->next = new;
+	new->next = tmp;
+	return (0);
+}
+
+int				ft_export(char **args, t_env *env)
+{
+	if (!args[1])
+		return (0);
+	else
+	{
+		env_add_back(args[1], env);
+		while (env && env->next)
+			env = env->next;
+	}
 	return (0);
 }
