c9b0faffc21d1d1d1d1c6a0c62aa267c09d91018
macrespo <maxime.crespopro@gmail.com>
2020-06-19 15:36:37 +0200

    cd - repaired

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/08 19:46:37 by macrespo          #+#    #+#             */
-/*   Updated: 2020/06/18 19:11:42 by macrespo         ###   ########.fr       */
+/*   Updated: 2020/06/19 15:27:28 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,31 +68,43 @@ static int		update_oldpwd(t_env *env)
 	return (0);
 }
 
+static int		go_to_path(const char *path, int option, t_env *env)
+{
+	int		ret;
+	char	*env_path;
+
+	if (option == 0)
+	{
+		update_oldpwd(env);
+		env_path = get_env_path(env, "HOME", 4);
+	}
+	else if (option == 1)
+	{
+		env_path = get_env_path(env, "OLDPWD", 6);
+		update_oldpwd(env);
+	}
+	if (option != 2)
+	{
+		ret = chdir(env_path);
+		free(env_path);
+		return (ret);
+	}
+	update_oldpwd(env);
+	ret = chdir(path);
+	return (ret);
+}
+
 int				ft_cd(char **args, t_env *env)
 {
 	int		cd_ret;
-	char	*env_path;
 
-	env_path = NULL;
-	update_oldpwd(env);
 	if (!args[1])
-	{
-		env_path = get_env_path(env, "HOME", 4);
-		cd_ret = chdir(env_path);
-	}
+		return (go_to_path(NULL, 0, env));
+	if (ft_strcmp(args[1], "-") == 0)
+		cd_ret = go_to_path(args[1], 1, env);
 	else
-	{
-		if (ft_strcmp(args[1], "-") == 0)
-		{
-			env_path = get_env_path(env, "OLDPWD", 6);
-			cd_ret = chdir(env_path);
-		}
-		else
-			cd_ret = chdir(args[1]);
-	}
+		cd_ret = go_to_path(args[1], 2, env);
 	if (cd_ret != 0)
 		print_error(args);
-	if (env_path)
-		free(env_path);
 	return (cd_ret);
 }
